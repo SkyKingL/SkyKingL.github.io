18a93638fbf898bef0de246211fd3992a2cf6d1b
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-11-18 10:28:58 +0000

    add google-site-verification

---
+++ scripts/google-site-verification.js
@@ -0,0 +1,3 @@
+hexo.extend.filter.register('theme_inject', function (injects) {
+    injects.head.raw('google-site-verification', '<meta name="google-site-verification" content="5tyL-1P6pgVjMW6Wwyx06vNCixQTsvEdzGR_UtfQ4co" />');
+});
